00195fa22cc33caba4d296e2a313c4ca849ec251
Phillip Passmore <phillipspassmore@gmail.com>
2023-07-28 20:58:09 +0100

    About page layout updated
    Signed-off-by: Phillip Passmore <phillipspassmore@gmail.com>

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
+import styles from './about.module.css';
 
 function About() {
   const { siteConfig } = useDocusaurusContext();
@@ -15,14 +15,17 @@ function About() {
           <p className="hero__subtitle">Hello and welcome to my website!</p>
         </div>
       </header>
-      <main>
-        <h2>Introduction</h2>
-        <p>In this blog, I am aiming to document and share my Raspberry Pi projects, electronics tinkering, coding adventures, and diving into the world of cloud computing. </p>
-        <p>As a computer science nerd, I enjoy spending my time working on personal projects to develop my skills</p>
+      <main className={styles.containerWithSidebar}>
+        <div className={styles.mainContent}>
+          <h2>Introduction</h2>
+          <p>In this blog, I am aiming to document and share my Raspberry Pi projects, electronics tinkering, coding adventures, and diving into the world of cloud computing. </p>
+          <p>As a computer science nerd, I enjoy spending my time working on personal projects to develop my skills</p>
 
-        <h2>Professional Experience</h2>
-        <p>Initially, I specialised in academic research, with a primary focus on climate change. I completed a PhD <a href="https://ore.exeter.ac.uk/repository/handle/10871/30021" target="_blank">'Consequences of communicating climate science online'</a> and undertook postdoctoral work in Politics.</p>
-        {/* Include additional content here */}
+          <h2>Professional Experience</h2>
+          <p>Initially, I specialised in academic research, with a primary focus on climate change. I completed a PhD <a href="https://ore.exeter.ac.uk/repository/handle/10871/30021" target="_blank">'Consequences of communicating climate science online'</a> and undertook postdoctoral work in Politics.</p>
+          
+        </div>
+        
       </main>
     </Layout>
   );
